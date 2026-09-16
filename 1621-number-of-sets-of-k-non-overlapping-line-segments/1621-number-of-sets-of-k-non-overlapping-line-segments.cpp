c5b50d33bdcae07fbbcac0e9529fbdaedf7e4bbc
Om kumar <omrajora671@gmail.com>
2026-09-16 19:16:47 +0530

    Time: 35 ms (64.49%), Space: 53 MB (34.11%) - LeetHub - Time: O(nk), Space: O(nk)

---
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numberOfSets(int n, int k) {
+         const int MOD = 1e9 + 7;
+        vector<vector<long long>> dp(n, vector<long long>(k + 1));
+    for(int i = 0; i < n; i++) {
+        dp[i][0] = 1;
+    }
+
+        for (int j = 1; j <= k; j++) {
+            long long sum = 0;
+
+            for (int i = 1; i < n; i++) {
+
+                sum = (sum + dp[i - 1][j - 1]) % MOD;
+
+                dp[i][j] = (dp[i - 1][j] + sum) % MOD;
+            }
+        }
+
+        return dp[n - 1][k];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
